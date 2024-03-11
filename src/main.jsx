664e3e2222d02e0cea5dda3c9df80df87599a53f
hloba-ukraine <138844087+hloba-ukraine@users.noreply.github.com>
2024-03-11 09:55:15 -0600

    second

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "modern-normalize";
 import App from "./components/App.jsx";
 import "./index.css";
+import "modern-normalize";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
